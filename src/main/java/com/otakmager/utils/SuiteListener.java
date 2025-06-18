47947f5b98662d17548161b1c570d516ba6c06ac
Dimas Wahyu Ardiyanto <dimazwahyuardiyanto@gmail.com>
2025-06-18 13:34:37 +0700

    feat: add SuiteListener

---
+++ src/main/java/com/otakmager/utils/SuiteListener.java
@@ -0,0 +1,41 @@
+package com.otakmager.utils;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.OutputType;
+import org.testng.IAnnotationTransformer;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+import org.testng.annotations.ITestAnnotation;
+
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+
+public class SuiteListener implements ITestListener, IAnnotationTransformer {
+
+    private static final Logger logger = LogManager.getLogger(SuiteListener.class);
+
+    @Override
+    public void onTestFailure(ITestResult result) {
+        String methodName = result.getMethod().getMethodName();
+        String fileName = System.getProperty("user.dir") + File.separator + "screenshots" + File.separator
+                + result.getMethod().getMethodName();
+        File file = DriverFactory.getDriver().getScreenshotAs(OutputType.FILE);
+
+        try {
+            FileUtils.copyFile(file, new File(fileName + ".png"));
+            logger.info("Screenshot captured for failed test: {}", methodName);
+            logger.info("Screenshot saved at: {}", fileName);
+        } catch (IOException e) {
+            logger.error("Failed to save screenshot for test: {}", methodName, e);
+        }
+    }
+
+    @Override
+    public void transform(ITestAnnotation annotation, Class testClass, Constructor testConstructor, Method testMethod) {
+        annotation.setRetryAnalyzer(RetryAnalyzer.class);
+    }
+}

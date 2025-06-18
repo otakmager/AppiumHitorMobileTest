81d21c66610fb2a5cc2fea7ba27bc5b34ce799c7
Dimas Wahyu Ardiyanto <dimazwahyuardiyanto@gmail.com>
2025-06-18 10:32:32 +0700

    feat: add ConfigReader

---
+++ src/main/java/com/otakmager/utils/ConfigReader.java
@@ -0,0 +1,34 @@
+package com.otakmager.utils;
+
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.InputStream;
+import java.util.Properties;
+
+public class ConfigReader {
+    private static final Logger logger = LogManager.getLogger(ConfigReader.class);
+    private static final Properties props = new Properties();
+
+    static {
+        try {
+            InputStream input = ConfigReader.class
+                    .getClassLoader()
+                    .getResourceAsStream("config.properties");
+
+            if (input == null) {
+                throw new RuntimeException("config.properties file not found");
+            } else {
+                props.load(input);
+                logger.info("Loaded config.properties successfully");
+            }
+        } catch (Exception e) {
+            logger.error("Failed to load config.properties: {}", e.getMessage(), e);
+        }
+    }
+
+    public static String getProperty(String key) {
+        return props.getProperty(key);
+    }
+}

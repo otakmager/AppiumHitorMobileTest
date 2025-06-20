319cfbd47a3c42a4580b32a1c15c7b1021e785d9
Dimas Wahyu Ardiyanto <dimazwahyuardiyanto@gmail.com>
2025-06-20 07:43:43 +0700

    feat: add HomeObject

---
+++ src/main/java/com/otakmager/pageObjects/HomeObject.java
@@ -0,0 +1,13 @@
+package com.otakmager.pageObjects;
+
+import com.otakmager.model.ElementInfo;
+import io.appium.java_client.AppiumBy;
+
+public interface HomeObject {
+    ElementInfo appTitle = new ElementInfo("App Title", AppiumBy.accessibilityId("Hitor"));
+    ElementInfo appDesc = new ElementInfo("App Desc", AppiumBy.androidUIAutomator("new UiSelector().description(\"Kendaraan lewat? Kita Catat!\")"));
+    ElementInfo menuPengamatan = new ElementInfo("Menu Pengamatan", AppiumBy.accessibilityId("Mulai Pengamatan"));
+    ElementInfo menuRiwayat = new ElementInfo("Menu Riwayat", AppiumBy.accessibilityId("Lihat Riwayat"));
+    ElementInfo menuPengaturan = new ElementInfo("Menu Pengaturan", AppiumBy.accessibilityId("Pengaturan"));
+    ElementInfo menuDukungan = new ElementInfo("Menu Dukungan", AppiumBy.accessibilityId("Dukung Kami"));
+}

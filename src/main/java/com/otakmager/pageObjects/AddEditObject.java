8eb05180a3b01b14c01e930b530dc7ef5672d4fe
Dimas Wahyu Ardiyanto <dimazwahyuardiyanto@gmail.com>
2025-06-20 09:02:54 +0700

    feat: add AddEditObject

---
+++ src/main/java/com/otakmager/pageObjects/AddEditObject.java
@@ -0,0 +1,16 @@
+package com.otakmager.pageObjects;
+
+import com.otakmager.model.ElementInfo;
+import io.appium.java_client.AppiumBy;
+
+public interface AddEditObject {
+    ElementInfo appBarTitleAdd = new ElementInfo("App Bar Title Add", AppiumBy.accessibilityId("Tambah Data"));
+    ElementInfo appBarTitleEdit = new ElementInfo("App Bar Title Edit", AppiumBy.accessibilityId("Edit Data"));
+    ElementInfo appBarBackButton = new ElementInfo("App Bar Back Button", AppiumBy.accessibilityId("Back"));
+    ElementInfo appBarSaveButton = new ElementInfo("App Bar Save Button", AppiumBy.androidUIAutomator("new UiSelector().className(\"android.widget.Button\").instance(1)"));
+    ElementInfo resultSummary = new ElementInfo("Result Summary", AppiumBy.androidUIAutomator("new UiSelector().descriptionContains(\"Ringkasan\")"));
+    ElementInfo resultRecord = new ElementInfo("Result Summary", AppiumBy.androidUIAutomator("new UiSelector().descriptionContains(\"Data Pengamatan\")"));
+    ElementInfo fieldPengamat = new ElementInfo("Field Pengamat", AppiumBy.androidUIAutomator("new UiSelector().className(\"android.widget.EditText\").instance(0)"));
+    ElementInfo fieldLokasi = new ElementInfo("Field Lokasi", AppiumBy.androidUIAutomator("new UiSelector().className(\"android.widget.EditText\").instance(1)"));
+    ElementInfo fieldKeterangan = new ElementInfo("Field Keterangan", AppiumBy.androidUIAutomator("new UiSelector().className(\"android.widget.EditText\").instance(2)"));
+}

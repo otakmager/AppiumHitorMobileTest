1f9ad81f8a52fbb9ea2c2555c66ae60d2e1a990d
Dimas Wahyu Ardiyanto <dimazwahyuardiyanto@gmail.com>
2025-06-22 21:18:28 +0700

    feat: add PilihJalanEvents

---
+++ src/main/java/com/otakmager/pageEvents/PilihJalanEvents.java
@@ -0,0 +1,32 @@
+package com.otakmager.pageEvents;
+
+import com.otakmager.keywords.ActionKeyword;
+import com.otakmager.keywords.ValidationKeyword;
+import com.otakmager.pageObjects.PilihJalanObject;
+
+public class PilihJalanEvents {
+    ActionKeyword action = new ActionKeyword();
+    ValidationKeyword validation = new ValidationKeyword();
+
+    public void verifyAppBarTitle() {
+        String expectedTitle = "Pilih Jenis Jalan";
+        String actualTitle = action.getText(PilihJalanObject.appBarTitle);
+        validation.assertTextEquals(actualTitle, expectedTitle);
+    }
+
+    public void verifyAppBarBackButtonVisible() {
+        validation.assertElementVisible(PilihJalanObject.appBarBackButton);
+    }
+
+    public void verifyMenuJalanRayaVisible() {
+        validation.assertElementVisible(PilihJalanObject.menuJalanRaya);
+    }
+
+    public void verifyMenuJalanTolVisible() {
+        validation.assertElementVisible(PilihJalanObject.menuJalanTol);
+    }
+
+    public void clickMenuJalanRaya() {
+        action.click(PilihJalanObject.menuJalanRaya);
+    }
+}

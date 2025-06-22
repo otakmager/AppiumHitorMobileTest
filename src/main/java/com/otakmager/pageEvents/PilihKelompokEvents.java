cf9c6bc07a78977d48c69051f6a638a33a942134
Dimas Wahyu Ardiyanto <dimazwahyuardiyanto@gmail.com>
2025-06-22 21:22:43 +0700

    feat: add PilihKelompokEvents

---
+++ src/main/java/com/otakmager/pageEvents/PilihKelompokEvents.java
@@ -0,0 +1,36 @@
+package com.otakmager.pageEvents;
+
+import com.otakmager.keywords.ActionKeyword;
+import com.otakmager.keywords.ValidationKeyword;
+import com.otakmager.pageObjects.PilihKelompokObject;
+
+public class PilihKelompokEvents {
+    ActionKeyword action = new ActionKeyword();
+    ValidationKeyword validation = new ValidationKeyword();
+
+    public void verifyAppBarTitle() {
+        String expectedTitle = "Pilih Kelompok";
+        String actualTitle = action.getText(PilihKelompokObject.appBarTitle);
+        validation.assertTextEquals(actualTitle, expectedTitle);
+    }
+
+    public void verifyAppBarBackButton() {
+        validation.assertElementVisible(PilihKelompokObject.appBarBackButton);
+    }
+
+    public void verifyMenu5TipeVisible() {
+        validation.assertElementVisible(PilihKelompokObject.menu5Tipe);
+    }
+
+    public void verifyMenu8TipeVisible() {
+        validation.assertElementVisible(PilihKelompokObject.menu8Tipe);
+    }
+
+    public void verifyMenu12TipeVisible() {
+        validation.assertElementVisible(PilihKelompokObject.menu12Tipe);
+    }
+
+    public void clickMenu5Tipe() {
+        action.click(PilihKelompokObject.menu5Tipe);
+    }
+}

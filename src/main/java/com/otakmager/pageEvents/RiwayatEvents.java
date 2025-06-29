793be003f14a2bb367cfcab6e6d1d8d9a9b6e282
Dimas Wahyu Ardiyanto <dimazwahyuardiyanto@gmail.com>
2025-06-29 17:00:33 +0700

    feat: add RiwayatEvents

---
+++ src/main/java/com/otakmager/pageEvents/RiwayatEvents.java
@@ -0,0 +1,26 @@
+package com.otakmager.pageEvents;
+
+import com.otakmager.keywords.ActionKeyword;
+import com.otakmager.keywords.ValidationKeyword;
+import com.otakmager.pageObjects.RiwayatObject;
+
+public class RiwayatEvents {
+    ActionKeyword action = new ActionKeyword();
+    ValidationKeyword validation = new ValidationKeyword();
+
+    public void verifyAppBarTitle() {
+        String expectedTitle = "Riwayat";
+        String actualTitle = action.getText(RiwayatObject.appBarTitle);
+        validation.assertTextEquals(actualTitle, expectedTitle);
+    }
+
+    public void verifyAppBarBackButtonVisible() {
+        validation.assertElementVisible(RiwayatObject.appBarBackButton);
+    }
+
+    public void verifyNoDataMessageVisible() {
+        String expectedTitle = "Tidak ada data";
+        String actualTitle = action.getText(RiwayatObject.noDataText);
+        validation.assertTextEquals(actualTitle, expectedTitle);
+    }
+}
